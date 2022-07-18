145a2409e6ea1eb0b4fcbd044d456b941d168408
Lauren <laurenkatemcpartland@gmail.com>
2022-07-18 09:57:08 +0100

    Updated icon

--- src/app.js
+++ src/app.js
@@ -25,17 +25,20 @@ let  currentDescription = document.querySelector("#description");
 let  currentHumidity = document.querySelector("#humidity");
 let  currentWind = document.querySelector("#windspeed");
 let  currentDate = document.querySelector("#date");
+let  weatherIcon = document.querySelector ("#icon");
 currentCity.innerHTML = response.data.name;
 currentTemperature.innerHTML = Math.round(response.data.main.temp);
 currentDescription.innerHTML = response.data.weather[0].description;
 currentHumidity.innerHTML = response.data.main.humidity;
 currentWind.innerHTML = Math.round(response.data.wind.speed);
 currentDate.innerHTML = formatDate(response.data.dt * 1000) ;
+weatherIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data. weather[0].icon}@2x.png`);
+weatherIcon.setAttribute("alt", response.data.weather[0].description);
 
 }
 
 
 let apiKey = "e304b015eb7af663852222eb9928a3f7";
 let city = "New York";
-let apiURL= `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric&wind.speed=imperial`;
+let apiURL= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&wind.speed=imperial`;
 axios.get(apiURL).then(showTemperature);
